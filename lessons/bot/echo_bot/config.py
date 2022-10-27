c1b3854d1769b8201b19d6b7452acf778a44732d
annasavasteeva <anna.savasteeva@gmail.com>
2022-10-27 23:36:27 +0300

    lesson 9

---
+++ lessons/bot/echo_bot/config.py
@@ -0,0 +1,1 @@
+TOKEN = '5690137384:AAHrXgHlm1xODYf3pcdzjUZ4aOrvap58kjM'
---
+++ lessons/bot/lesson9.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="свой токен")
+# Диспетчер
+dp = Dispatcher(bot)
+
+# Хэндлер на команду /start
+@dp.message_handler(commands=["start", 'Hello'])
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
